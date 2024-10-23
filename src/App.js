7b5dd1a0d13ced1fa3b09d76bb03536dc887b1fc
easycodewithme <pulse.rajan22@gmail.com>
2024-10-23 14:03:44 +0530

    apisolved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-
+import AnimatedCursor from "react-animated-cursor";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
+// import { ThreeDCardDemo } from "./ThreeDCardDemo"; // Import the 3D card demo component
 
-const API_URL = 'http://www.omdbapi.com/?apikey=8d989f13'
+const API_URL = 'https://www.omdbapi.com/?apikey=8d989f13';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -17,13 +18,34 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
     setMovies(data.Search);
   };
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
+
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={40}
+        color="255,255,255"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
+        ]}
+      />
 
       <div className="search">
         <input
@@ -41,7 +63,7 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
@@ -49,9 +71,10 @@ const App = () => {
           <h2>No movies found</h2>
         </div>
       )}
+
+     
     </div>
   );
 };
-
 
 export default App;
